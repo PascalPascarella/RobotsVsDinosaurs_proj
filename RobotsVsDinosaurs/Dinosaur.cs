37a41c82d3636a99f4d080a056ea57e578c904af
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-09-16 12:44:34 -0500

    Implement randomizer to energy at object creation

--- RobotsVsDinosaurs/Dinosaur.cs
+++ RobotsVsDinosaurs/Dinosaur.cs
@@ -7,9 +7,9 @@ namespace RobotsVsDinosaurs
 	class Dinosaur
 	{
 		// Member
-		public string name;								// Assigned to dinosaur at new class instance
-		public double health;							// Health bar that decreases upon taking hit
-		public double energy;							// Energy bar that decreases with each attack exertion
+		public string name;									// Assigned to dinosaur at new class instance
+		public double health;								// Health bar that decreases upon taking hit
+		public double energy;								// Energy bar that decreases with each attack exertion
 		public AttackType[] attackType;     // Creates random attack
 		public Random randomizer = new Random();
 
@@ -18,21 +18,21 @@ namespace RobotsVsDinosaurs
 		{
 			name = newInstance;
 			health = 10 + RandomHealth();
-			energy = 100;
-			Console.WriteLine("A new dinosaur, '{0},' has been created with health of {1}!", newInstance);
+			energy = 100 +RandomEnergy();
+			Console.WriteLine("A new dinosaur, '{0},' has been created with health of {1} and {2} energy!", newInstance, health, energy);
 		}
 
 		// Member Methods
 		// Small Randomizer
 		public int RandomHealth()
 		{
-			return randomizer.Next(1, 10);
+			return randomizer.Next(1, 10);			// Returns random integer value between 1 and 10 (inclusive)
 		}
 
 		// Large Randomizer
-		public void RandomEnergy()
+		public int RandomEnergy()
 		{
-
+			return randomizer.Next(1, 50);      // Returns random integer value between 1 and 50 (inclusive)
 		}
 	}
 }
--- RobotsVsDinosaurs/Robot.cs
+++ RobotsVsDinosaurs/Robot.cs
@@ -7,9 +7,9 @@ namespace RobotsVsDinosaurs
 	class Robot
 	{
 		// Member Variables
-		public string name;								// Assigned to robot object at new class instance
-		public double integrity;					// Health bar that decreases upon taking hit
-		public double powerLevel;					// Energy bar that decreases with each attack exertion
+		public string name;											// Assigned to robot object at new class instance
+		public double integrity;								// Health bar that decreases upon taking hit
+		public double powerLevel;								// Energy bar that decreases with each attack exertion
 		public List<WeaponType> weaponType;     // Creates random attack combinations
 		public Random randomizer = new Random();
 
@@ -18,20 +18,20 @@ namespace RobotsVsDinosaurs
 		{
 			name = newInstance;
 			integrity = 10 + RandomIntegrity();
-			powerLevel = 100;
-			Console.WriteLine("A new robot, 'Servitor {0},' has been created with health of {1}!", newInstance, integrity);
+			powerLevel = 100 + RandomPowerLevel();
+			Console.WriteLine("A new robot, 'Servitor {0},' has been created with integrity of {1} and {2} Watt-hours of power!", newInstance, integrity, powerLevel);
 		}
 
 		// Member Methods
 		// Small Randomizer
 		public int RandomIntegrity()
 		{
-			return randomizer.Next(1, 10);
+			return randomizer.Next(1, 10);      // Returns random integer value between 1 and 10 (inclusive)
 		}
 		// Large Randomizer
-		public void RandomPowerLevel()
+		public int RandomPowerLevel()
 		{
-
+			return randomizer.Next(1, 50);      // Returns random integer value between 1 and 50 (inclusive)
 		}
 	}
 }
