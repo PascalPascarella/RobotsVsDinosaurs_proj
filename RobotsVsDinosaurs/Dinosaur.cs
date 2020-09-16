cd916286f1fc6c5cbf92bf6e69332437c660b8ba
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-09-16 12:25:57 -0500

    Implement randomizer to health at object creation and prepare attackType as array, weaponType as list

--- RobotsVsDinosaurs/Dinosaur.cs
+++ RobotsVsDinosaurs/Dinosaur.cs
@@ -10,22 +10,23 @@ namespace RobotsVsDinosaurs
 		public string name;								// Assigned to dinosaur at new class instance
 		public double health;							// Health bar that decreases upon taking hit
 		public double energy;							// Energy bar that decreases with each attack exertion
-		public AttackType attackType;     // Creates random attack 
+		public AttackType[] attackType;     // Creates random attack
 		public Random randomizer = new Random();
 
 		// Constructor
 		public Dinosaur(string newInstance)
 		{
 			name = newInstance;
-			health = 10;
+			health = 10 + RandomHealth();
 			energy = 100;
-			Console.WriteLine("A new dinosaur, '{0},' has been created!", newInstance);
+			Console.WriteLine("A new dinosaur, '{0},' has been created with health of {1}!", newInstance);
 		}
 
 		// Member Methods
 		// Small Randomizer
-		public void RandomHealth()
+		public int RandomHealth()
 		{
+			return randomizer.Next(1, 10);
 		}
 
 		// Large Randomizer
--- RobotsVsDinosaurs/Robot.cs
+++ RobotsVsDinosaurs/Robot.cs
@@ -10,22 +10,23 @@ namespace RobotsVsDinosaurs
 		public string name;								// Assigned to robot object at new class instance
 		public double integrity;					// Health bar that decreases upon taking hit
 		public double powerLevel;					// Energy bar that decreases with each attack exertion
-		public WeaponType weaponType;     // Creates random attack combinations
+		public List<WeaponType> weaponType;     // Creates random attack combinations
 		public Random randomizer = new Random();
 
 		// Constructor
 		public Robot(string newInstance)
 		{
 			name = newInstance;
-			integrity = 10;
+			integrity = 10 + RandomIntegrity();
 			powerLevel = 100;
-			Console.WriteLine("A new robot, 'Servitor {0},' has been created!", newInstance);
+			Console.WriteLine("A new robot, 'Servitor {0},' has been created with health of {1}!", newInstance, integrity);
 		}
 
 		// Member Methods
 		// Small Randomizer
-		public void RandomIntegrity()
+		public int RandomIntegrity()
 		{
+			return randomizer.Next(1, 10);
 		}
 		// Large Randomizer
 		public void RandomPowerLevel()
